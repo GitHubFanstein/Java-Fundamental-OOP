d18d563b94ceb2901004432d0c408e46d51471dc
GitHubFanstein <158123195+GitHubFanstein@users.noreply.github.com>
2024-07-18 22:49:09 +0700

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -3,28 +3,46 @@
  */
 
 public class Employee extends user{
-    String userName[];
-    String userPassword[];
-    
-    public Employee(String nameEmployee, String passEmployee, String[] userName, String[] userPassword) {
-        super(nameEmployee, passEmployee);
-        this.userName = userName;
-        this.userPassword = userPassword;
+    private String nameEmployee;
+
+    public Employee(String userName, String userPassword, String nameEmployee) {
+        super(userName, userPassword);
+        this.nameEmployee = nameEmployee;
     }
-    public String[] getUserName() {
-        return userName;
+
+
+    @Override
+    public void checkUserLogin(){
+
+      if(Employee.super.getUserPassword() == "Aten123" && Employee.super.getUserName() == "Aten"){
+        System.out.println("berhasil Login " + nameEmployee);
+      } else{
+        System.out.println("gagal Login " + nameEmployee);
+      }
     }
-    public void setUserName(String[] userName) {
-        this.userName = userName;
-    }
-    public String[] getUserPassword() {
-        return userPassword;
-    }
-    public void setUserPassword(String[] userPassword) {
-        this.userPassword = userPassword;
-    }
-    
-    
 
     
+
+    public String getNameEmployee() {
+        return nameEmployee;
+    }
+
+    public void setNameEmployee(String nameEmployee) {
+        this.nameEmployee = nameEmployee;
+    }
+
+    public void leaveApplication(String empID) {
+        String[][] inputdata = {
+            {empID, "Annual Leave", "2024-07-20", "2024-07-30", "Pending", "Vacation", "10", nameEmployee} 
+          };
+
+          String[][] applicationResult = user.leaveApplication(inputdata);
+
+    for(int i = 0; i < applicationResult.length; i++){
+        for (int j = 0; j < applicationResult[i].length; j++) {
+            System.out.println(applicationResult[i][j]) ;
+        }
+        System.out.println();
+}
+    }
 }
--- MainView.java
+++ MainView.java
@@ -1,8 +1,17 @@
 public class MainView {
     public static void main(String[] args) {
-        user userInstance = new user("jono", "aten");
-        userInstance.checkUserLogin();
-        userInstance.leaveApplication();
-        userInstance.tesStop();
+       
+      user user = new user(null, null);
+
+      user.checkUserLogin();
+
+    
+      Employee employee = new Employee("Aten", "Aten123", "Roni");
+
+      employee.checkUserLogin();
+      employee.leaveApplication("11092");
+      employee.leaveApplication("12333");
+
+     
     }
-}
+}   
