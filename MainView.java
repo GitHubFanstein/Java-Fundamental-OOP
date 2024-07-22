bf66a587badcf957c0be0fcc591f63de2a10a915
GitHubFanstein <158123195+GitHubFanstein@users.noreply.github.com>
2024-07-22 14:31:30 +0700

    Add files via upload

--- MainView.java
+++ MainView.java
@@ -1,8 +1,24 @@
 public class MainView {
     public static void main(String[] args) {
-        user userInstance = new user("jono", "aten");
-        userInstance.checkUserLogin();
-        userInstance.leaveApplication();
-        userInstance.tesStop();
+       
+      user user = new user("Kami", "Kami123");
+
+    //   user.login();
+
+        
+
+    //  Employee employee = new Employee(user.getUserName(), user.getUserPassword(), "Child", "IT");
+     Employee employee2 = new Employee(user.getUserName(), user.getUserPassword(), "TIm", "Finance");
+
+    
+
+    //     employee.login();  
+
+    //   employee.leaveApplication("11092");
+
+    employee2.leaveApplication2("9000", "Annual Leave", "2024-07-20", "2024-07-30", "Pending", "Vacation", "10", "Aten");
+    
+    employee2.leaveApplication2("10101", "Annual Leave", "2024-07-20", "2024-07-30", "Accept", "Vacation", "10", "Rifa");
+    
     }
-}
+}   
--- user.java
+++ user.java
@@ -1,36 +1,21 @@
 
-public class user implements JavaOOPInterface  {
+class user implements JavaOOPInterface {
     private String userName;
     private String userPassword;
 
-    
 
-    public user(String nameEmployees, String passEmployees) {
-        this.userName = nameEmployees;
-        this.userPassword = passEmployees;
+    public user(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
     }
 
-    public void checkUserLogin() {
-        String[] userNames = {"Aten","jono"};
-        String[] userPasswords = {"aten"};
-
-        Employee employee = new Employee(userPassword, userName, userNames, userPasswords);
-
-        for (int i = 0; i < employee.getUserName().length; i++) {
-            if (employee.getUserName()[i] == this.userName && employee.getUserPassword()[i] == this.userPassword) {
-                System.out.println("Username dan Password Benar");
-                return;
-            }
-        }
-        System.out.println("Password dan Username Salah");
-    }
-
+   
     public static String[][] leaveApplication(String[][] inputData ) {
 
         int numberOfApplication = inputData.length;
-        String[][] applicationMessage = new String[numberOfApplication][7];
+        String[][] applicationMessage = new String[numberOfApplication][8];
 
-        String[] labels = {"Employee ID: ", "Jenis Cuti: ", "Tanggal Awal Cuti: ", "Tanggal Akhir Cuti: ", "Status: ", "Keterangan Cuti: ", "Jumlah Pengajuan Cuti: "};
+        String[] labels = {"Employee ID: ", "Jenis Cuti: ", "Tanggal Awal Cuti: ", "Tanggal Akhir Cuti: ", "Status: ", "Keterangan Cuti: ", "Jumlah Pengajuan Cuti: " , "Nama : " };
 
         for (int i = 0; i < numberOfApplication; i++) {
             for (int o = 0; o < labels.length; o++) {
@@ -40,31 +25,57 @@ public class user implements JavaOOPInterface  {
         return applicationMessage;
     }
 
-    public void  tesStop(){
-        System.out.println("Exit Program");
+    public String getUserName() {
+        return userName;
+    }
+
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    @Override
+    public void login(){
+       if(this.userName == getUserName() && this.getUserPassword() == getUserPassword()){
+        System.out.println("success");
+       }else{
+        System.out.println("Not Success");
+       }
     }
 
 
     @Override
-    public void leaveApplication() {
+    public void tesStop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tesStop'");
+    }
+
+    @Override
+    public void leaveApplication() {    
         String[][] inputdata = {
-            {"E12345", "Annual Leave", "2024-07-20", "2024-07-30", "Pending", "Vacation", "10"},
-            {"E12346", "Sick Leave", "2024-08-01", "2024-08-05", "Approved", "Flu", "5"},
-            {"E12347", "Maternity Leave", "2024-09-01", "2024-12-01", "Pending", "Childbirth", "90"},
-            {"E12348", "Annual Leave", "2024-10-15", "2024-10-20", "Pending", "Family Trip", "5"},
-            {"E12349", "Sick Leave", "2024-11-01", "2024-11-10", "Approved", "Surgery", "10"},
-            {"E12350", "Annual Leave", "2024-12-15", "2024-12-25", "Pending", "Holiday", "10"}
+            {"123", "Annual Leave", "2024-07-20", "2024-07-30", "Pending", "Vacation", "10", userName} 
           };
 
           String[][] applicationResult = user.leaveApplication(inputdata);
 
     for(int i = 0; i < applicationResult.length; i++){
         for (int j = 0; j < applicationResult[i].length; j++) {
-            System.out.println(applicationResult[i][j]);
+            System.out.println(applicationResult[i][j]) ;
         }
         System.out.println();
 }
     }
-    
+   
+    }
 
-}
+    
